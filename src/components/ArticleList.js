fb4f88de39b802b2c59ffea4fa457193bf3ee27b
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-03-07 15:22:23 -0600

    commit tree d0169ec231deeac01126baed55795e199655507e

---
+++ src/components/ArticleList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Article from "./Article"
+
+function ArticleList({posts}){
+    //console.log(posts)
+    const artArr = posts.map(post => {
+        return <Article key={post.id} title={post.title} date={post.date} preview={post.preview}/>
+    })
+
+    return(
+        <main>
+            {artArr}
+        </main>
+    )
+}
+
+export default ArticleList;
